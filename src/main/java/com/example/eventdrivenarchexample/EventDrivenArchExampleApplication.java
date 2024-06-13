97ac32146d4cb38e5d81fea75b07faa3ea634606
Marcelo <marcelo17082000@gmail.com>
2024-06-12 21:25:15 -0300

    Created project

---
+++ src/main/java/com/example/eventdrivenarchexample/EventDrivenArchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventdrivenarchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenArchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenArchExampleApplication.class, args);
+	}
+
+}
